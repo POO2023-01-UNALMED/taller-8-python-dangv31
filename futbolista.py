d06ff096ac5da51409ec0495309a9bc9fd736689
dangv31 <dangiraldova@unal.edu.co>
2023-04-17 16:59:14 -0500

    ahorasisisi

--- futbolista.py
+++ futbolista.py
@@ -9,7 +9,7 @@ class Futbolista (Persona, Deportista):
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
-        Futbolista.listaFutbolistas(self)
+        Futbolista.listaFutbolistas.append(self)
     
     def __str__(self):
         return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
